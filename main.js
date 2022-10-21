6992cc0861d6787424477a11f6f85cd142966f29
hatane-rgb <sonson0825@outlook.jp>
2022-10-21 20:11:19 +0900

    やったぜ
    やっとできた

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("dialog").style.display ="none";
+function hjyan(){
+    $('.dia').fadeIn();
+		dialog.style.display ="block";
+	}
+    function toziru(){
+		dialog.style.display ="none";
+        $('.dia').fadeOut();
+    }
+
+
