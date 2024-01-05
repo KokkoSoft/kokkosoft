e5ee6016fde495d1a03f0a8c350958bdde77aa97
ふらふらめんたいこ <ske8103@outlook.jp>
2024-01-06 01:45:16 +0900

    改造a
    nankaumakuikanakatta

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-document.getElementById("dialog").style.display ="none";
-dia.style.position = "absolute";
+document.addEventListener("DOMContentLoaded", function() {
+  var backgroundnum = Math.floor(Math.random() * 9) + 1;
+  var background = "./imge/background/" + backgroundnum + ".png";
+        
+  var firstSticky = document.getElementById("FirstSticky");
+  firstSticky.style.backgroundImage = "url('" + background + "')";
+})
+
+var dialog = document.getElementById("dialog");
+dialog.style.display ="none";
 function hjyan(){
     $('.dia').fadeIn();
 		dialog.style.display ="block";
