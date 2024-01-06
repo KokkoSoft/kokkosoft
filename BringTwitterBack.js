7ca608b59f03ed1299fdfa62743146d5fc7e1ffc
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-07 00:37:20 +0900

    i thought wrongy-wrong

--- BringTwitterBack.js
+++ BringTwitterBack.js
@@ -1,13 +1,13 @@
 'use strict'
 /* BringTwitterBack(); */
 var x0 = 0; var x0rx = 0; var x0ry = 0; var x0rs = 0;
-const x0t = document.getElementById("x0").textContent;
-const x0s = document.getElementById("Xing0").style;
+const x0t = document.getElementById("x0");
+const x0s = document.getElementById("Xing0");
 
 function BringTwitterBack0() {
 	x0 += 1; x0rs = x0 * 1.2;
   if (x0 >= 12) {
-    x0t = "Twitter";
+    x0t.textContent = "Twitter";
   }
 }
 
@@ -15,7 +15,7 @@ function BringTwitterBackRunningBg() {
 	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
 	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
   x0rs -= x0rs / 100;
-  x0s.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
+  x0s.style.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
 }
 
 setInterval(BringTwitterBackRunningBg, 15);
