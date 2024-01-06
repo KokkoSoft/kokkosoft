2672b96588c4ecfb529352cd4c6edf4781c5ebb5
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-07 00:00:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function otu(){
 var x0 = 0; var x0rx = 0; var x0ry = 0; var x0rs = 0;
 var x0t = document.getElementById("x0").textContent;
 var x0s = document.getElementById("Xing0").style;
+
 function BringTwitterBack0() {
 	x0 += 1; x0rs = x0 * 1.2;
   if (x0 >= 12) {
@@ -32,10 +33,10 @@ function BringTwitterBack0() {
 }
 
 function BringTwitterBackRunningBg() {
-	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1) - (x0rs + 1));
-	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1) - (x0rs + 1));
+	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
+	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
   x0rs -= x0rs / 100;
-  x0s.transform = "translate(" + x0rx + "px, " + x0ry + "px)"
+  x0s.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
 }
 
 setInterval(BringTwitterBackRunningBg, 15);
