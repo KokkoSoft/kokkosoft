6c5e9e2a04bc38fad8d1857f8144f2b15bd3aaa0
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-06 23:17:35 +0900

    bringtwitterback isnt finished yet

--- main.js
+++ main.js
@@ -19,3 +19,13 @@ function hjyan(){
 function otu(){
   alert("作成中です。");
 }
+
+/* BringTwitterBack(); */
+var x0 = 0; var x0rx = 0; var x0ry = 0; var x0rs = 0;
+var x0t = document.getElementById("x0").textContent;
+var x0s = document.getElementById("x0").style;
+function BringTwitterBack0() {
+	x0 += 1; x0rs = x0 * 2;
+	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1) - (x0rs + 1);
+	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1) - (x0rs + 1);
+}
