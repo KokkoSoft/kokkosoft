a59e36e2197fb916f17dc5c120653e5ec30ed060
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-07 00:22:16 +0900

    okay move

--- main.js
+++ main.js
@@ -20,23 +20,3 @@ function otu(){
   alert("作成中です。");
 }
 
-/* bringTwitterBack(); */
-var x0 = 0; var x0rx = 0; var x0ry = 0; var x0rs = 0;
-var x0t = document.getElementById("x0").textContent;
-var x0s = document.getElementById("Xing0").style;
-
-function bringTwitterBack0() {
-	x0 += 1; x0rs = x0 * 1.2;
-  if (x0 >= 12) {
-    x0t = "Twitter";
-  }
-}
-
-function bringTwitterBackRunningBg() {
-	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
-	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
-  x0rs -= x0rs / 100;
-  x0s.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
-}
-
-setInterval(bringTwitterBackRunningBg, 15);
