a246c3ffc6977f6def54906e5fc02d27c539db46
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-06 23:25:27 +0900

    BringTwitterBack();

--- main.js
+++ main.js
@@ -26,6 +26,16 @@ var x0t = document.getElementById("x0").textContent;
 var x0s = document.getElementById("x0").style;
 function BringTwitterBack0() {
 	x0 += 1; x0rs = x0 * 2;
+  if (x0 >= 12) {
+    x0t = "Twitter";
+  }
+}
+
+function BringTwitterBackRunningBg() {
 	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1) - (x0rs + 1);
 	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1) - (x0rs + 1);
+  x0rs -= x0rs / 100;
+  x0s.transform = "translate(" + x0rx + "px, " + x0ry + "px)"
 }
+
+setInterval(5, BringTwitterBackRunningBg);
