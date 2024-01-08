4103ca5845dce8ac10addaca2825c82e26066e16
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-08 10:20:36 +0900

    Update BringTwitterBack.js

--- BringTwitterBack.js
+++ BringTwitterBack.js
@@ -5,7 +5,7 @@ const x0t = document.getElementById("Xing0");
 const x0s = document.getElementById("x0");
 
 function BringTwitterBack0() {
-	x0 += 1; x0rs = x0 * 1.2;
+	x0 += 1; x0rs = x0 * 2.5;
   if (x0 >= 12) {
     x0t.textContent = "Twitter";
   }
@@ -16,6 +16,6 @@ setInterval(BringTwitterBackRunningBg, 15);
 function BringTwitterBackRunningBg() {
 	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
 	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
-  x0rs -= x0rs / 100;
+  x0rs -= x0rs / 2000;
   x0s.style.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
 }
