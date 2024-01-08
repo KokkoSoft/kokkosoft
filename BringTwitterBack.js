7e320546a9a6d9a14f95bfe546f758affa395879
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-09 04:47:15 +0900

    Update BringTwitterBack.js

--- BringTwitterBack.js
+++ BringTwitterBack.js
@@ -1,14 +1,21 @@
 'use strict'
 /* BringTwitterBack(); */
 var x0 = 0; var x0rx = 0; var x0ry = 0; var x0rs = 0;
+var x0c = 0;
 const x0t = document.getElementById("Xing0");
 const x0s = document.getElementById("x0");
 const ixBwStylist = document.getElementById("ixBw").style;
 
 function BringTwitterBack0() {
-	x0 += 1; x0rs = x0 * 5;
-  if (x0 >= 12) {
+	x0 += 1; x0c += 1; x0rs = x0 * 5;
+  if (x0c === 12) {
     x0t.textContent = "Twitter";
+  } else if (x0c === 24) {
+    for (var x0tmp1 = 0; x0tmp1 <= 7; x0tmp1++) {
+      x0t.textContent.slice(0, (x0t.textContent.length - 1));
+    }
+    x0t.textContent = "X";
+    x0c = 0;
   }
 }
 
