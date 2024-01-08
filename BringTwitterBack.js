85dfa63efeac2014ef607227fed1d1eb8b638692
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-08 10:59:32 +0900

    Update BringTwitterBack.js

--- BringTwitterBack.js
+++ BringTwitterBack.js
@@ -19,5 +19,5 @@ function BringTwitterBackRunningBg() {
 	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
   x0rs -= x0rs / 2000;
   // x0s.style.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
-  ixbwStylist.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
+  ixBwStylist.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
 }
