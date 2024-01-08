abc6ab24d999670b1a95be98137a0c728390723c
Dripalse <85550737+D4p5-c47@users.noreply.github.com>
2024-01-08 10:54:48 +0900

    Update BringTwitterBack.js

--- BringTwitterBack.js
+++ BringTwitterBack.js
@@ -3,9 +3,10 @@
 var x0 = 0; var x0rx = 0; var x0ry = 0; var x0rs = 0;
 const x0t = document.getElementById("Xing0");
 const x0s = document.getElementById("x0");
+const ixBwStylist = document.getElementById("ixBw").style
 
 function BringTwitterBack0() {
-	x0 += 1; x0rs = x0 * 2.5;
+	x0 += 1; x0rs = x0 * 25;
   if (x0 >= 12) {
     x0t.textContent = "Twitter";
   }
@@ -17,5 +18,6 @@ function BringTwitterBackRunningBg() {
 	x0rx = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
 	x0ry = Math.floor(Math.random() * (x0rs * 2 + 1)) - (x0rs + 1);
   x0rs -= x0rs / 2000;
-  x0s.style.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
+  // x0s.style.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
+  ixbwStylist.transform = "translate(" + x0rx + "px, " + x0ry + "px)";
 }
